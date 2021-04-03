1ebfb2ac6bf145b54038ff5f31c79135ffed5f59
nicoKossmann <n.22koss@gmail.com>
2021-04-03 21:00:16 +0200

    changes

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# RSA
+# RSA :closed_lock_with_key:
+
+Implementierung der Lehrbuch RSA-Verschlüsselung
+
+## Inhaltsverzeichnis
+
+  - [Motivation](#motivation)
+  - [Nutzung](#nutzung)
+  - [Nützliche Links](#nützliche-links)
+
+## Motivation
+
+Zur Ergänzung der Lecture Notes im Modul *Grundlagen der IT-Sicherheit* habe ich ein Tool erstellt, welches die verschiedenen Schritte der Lehrbuch RSA-Verschlüsselung veranschaulichen soll. Dabei orientiert sich die Implementierung grob an dem Algorithmen des [Matheprismas](#nützliche-links) der Universität Wuppertal.
+
+## Nutzung
+Zum herunterladen:
+````
+$ git clone https://github.com/nicokossmann/RSA
+````
+
+## Nützliche Links
+
+* [Matheprisma](http://www.matheprisma.uni-wuppertal.de/Module/RSA/index.html)
--- rsa.py
+++ rsa.py
@@ -23,61 +23,60 @@ def extended_euclid(a, b):
     return (gcd, x - (b//a) * y, y)
 
 def mod_inverse(a, b):
-    g, x, y = extended_euclid(a, b)
-    if g != 1:
-        raise Exception("Keine multiplikative Inverse")
+    gcd, x, y = extended_euclid(a, b)
+    if gcd != 1:
+        raise Exception("Keine multiplikative Inverse gefunden!")
     return x%b
 
 def get_random_gcd(phi):
     e = random.randrange(2, phi)
     while(euclid(e, phi) != 1):
         e = random.randrange(2, phi)
-    return e
+    return e 
 
 def create_keys(p, q):
-    n = p*q
+    N = p*q
     phi = (p-1)*(q-1)
     e  = get_random_gcd(phi)
-    #e = 23
     d = mod_inverse(e, phi)
-    return ((e, n), (d, n))
+    return ((e, N), (d, N))
 
-def encrypt(m, e, n):
-    return (m**e)%n
+def encrypt(m, e, N):
+    return (m**e)%N
 
-def decrypt(c, d, n):
-    return (c**d)%n
+def decrypt(c, d, N):
+    return (c**d)%N
 
 def main():
-    print("\nWILLKOMMEN ZUM RSA VER-/ENTSCHLÜSSELUNGS-TOOL")
-    print("---------------------------------------------\n")
     try:
-       p = int(input("Geben Sie eine Primzahl 'p' an: "))
-       q = int(input("Geben sie eine Primzahl 'q' an: "))
+        print("\nWILLKOMMEN ZUM RSA VER-/ENTSCHLÜSSELUNGS-TOOL")
+        print("---------------------------------------------\n")
+        p = int(input("Geben Sie eine Primzahl 'p' an: "))
+        q = int(input("Geben sie eine Primzahl 'q' an: "))
+        if check_prim(p) and check_prim(q):
+            public_key, private_key = create_keys(p, q)
+            print("\nPublic-Key: {}\nPrivate-Key: {}\n".format(public_key, private_key))
+            e, N = public_key
+            d, N = private_key
+            choose = input("Geben Sie 'enc' zum Verschlüsseln und 'dec' zum Entschlüsseln der Nachricht an oder 'exit' zum beenden: ")
+            while(choose != 'exit'):
+                if choose == 'enc':
+                    m = int(input("Geben Sie eine Nachricht 'm' an: "))
+                    c = encrypt(m, e, N)
+                    print("\nChiffre 'c' lautet: {}\n".format(c))
+                elif choose == 'dec':
+                    c = int(input("Geben Sie eine Chiffre 'c' an: "))
+                    m = decrypt(c, d, N)
+                    print("\nNachricht 'm' lautet: {}\n".format(m))
+                else:
+                    print("\nFalsche Eingabe!\n")
+                choose = input("Geben Sie 'enc' zum Verschlüsseln und 'dec' zum Entschüsseln der Nachricht an oder 'exit' zum beenden: ")
+        else:
+            print("\n'p' oder 'q' sind keine Primzahl!\n")
+        print("\n")
     except ValueError:
-          print('Eingabe kein Integer\n')
-          return
-    if check_prim(p) and check_prim(q):
-        public_key, private_key = create_keys(p, q)
-        print("\nPublic Key: {}\nPrivate-Key: {}\n".format(public_key, private_key))
-        e, n = public_key
-        d, n = private_key
-        choose = input("Geben Sie 'enc' zum Verschlüsseln und 'dec' zum Entschlüsseln der Nachricht an oder 'exit' zum beenden: ")
-        while(choose != 'exit'):
-            if choose == 'enc':
-                m = int(input("Geben Sie eine Nachricht 'm' an: "))
-                c = encrypt(m, e, n)
-                print("\nChiffre 'c' lautet: {}\n".format(c))
-            elif choose == 'dec':
-                c = int(input("Geben Sie eine Chiffre 'c' an: "))
-                m = decrypt(c, d, n)
-                print("\nNachricht 'm' lautet: {}\n".format(m))
-            else:
-                print("Falsche Eingabe!")
-            choose = input("Geben Sie 'enc' zum Verschlüsseln und 'dec' zum Entschüsseln der Nachricht an oder 'exit' zum beenden: ")
-    else:
-        print("'p' oder 'q' sind keine Primzahl!")
-    print("\n")
+        print('\nKein Integer!\n')
+        return
     
 if __name__ == "__main__":
     main()
